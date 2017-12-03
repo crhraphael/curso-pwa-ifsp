bb3e5ad69f57d4c4634b00f829891df72673340f
Crhistian Raphael <crhistian_raphael@hotmail.com>
2017-12-03 18:07:00 -0300

    initial commit

---
+++ js/barcode.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+    $('.modal').modal(); //habilitando a janela modal
+});
+$('.code-scan').on('click', function() { //quando clicar no elemento com a classe "code-scan"
+    Quagga.init({ //configuração inicial mínima do Quagga
+        inputStream : {
+            name : "Live",
+            type : "LiveStream"
+        },
+        decoder : {
+            readers : ["code_128_reader"]
+        },
+    }, function(err) {
+        if (err) {
+            console.log(err);
+            return
+        }
+        console.log("Initialization finished. Ready to start");
+        Quagga.start();
+    });
+
+    Quagga.onDetected(function(result) { //quando o Quagga conseguir detectar um codigo de barras
+        var code = result.codeResult.code; //pega o código
+        console.log('Achei código de barras', code); //apenas printa no console
+        Quagga.stop(); //para a biblioteca
+        $('#scan').modal('close'); //fecha o modal
+        $('#codigo_ticket').focus().val(code); //coloca o valor do código no campo
+    });
+});
+
+/* notification */
+// precisa registrar o service worker
+navigator.serviceWorker.register('sw.js');
+
+$('body').on('submit', 'form', function(){
+    //notification do materialize
+    Materialize.toast('Toast!!', 4000);
+
+    var title = 'Pagamento confirmado';
+    var options = {
+        icon: 'img/icon.png', 
+        body: 'Saída liberada'
+    };
+
+    if ('Notification' in window) {
+        Notification.requestPermission();
+
+        if ('showNotification' in ServiceWorkerRegistration.prototype) {
+            console.log('Notification SW');
+            navigator.serviceWorker.ready.then(function(registration){
+                registration.showNotification(title, options);
+            });
+        } else {
+            console.log('Notification classic');
+            new Notification(title, options);
+        }
+    }
+
+    return false;
+});
