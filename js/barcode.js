5c8f8146f689999e51ac7ca7727c43fc66f907ce
Crhistian Raphael <crhistian_raphael@hotmail.com>
2017-12-04 17:40:05 -0300

    teste sem tamanho fixo

--- js/barcode.js
+++ js/barcode.js
@@ -5,11 +5,7 @@ $('.code-scan').on('click', function() { //quando clicar no elemento com a class
     Quagga.init({ //configuração inicial mínima do Quagga
         inputStream : {
             name : "Live",
-            type : "LiveStream",
-            constraints: {
-                width: $(window).width(),
-                height: 255
-            }
+            type : "LiveStream"
         },
         decoder : {
             readers : ["code_128_reader"]
